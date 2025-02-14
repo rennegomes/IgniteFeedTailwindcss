e154884516c147b30634d238616f806848c2a9f6
rennegomes <renne.lace082@gmail.com>
2025-02-14 15:43:25 -0300

    Fixed: sintaxe props em Post

--- app/components/Post.tsx
+++ app/components/Post.tsx
@@ -8,21 +8,21 @@ interface PostProps{
     fotoUrl: string
 }
 
-export function Post(prop: PostProps){
+export function Post(props: PostProps){
     return(
         <article className="bg-zinc-800 p-10 rounded-lg mt-8 max-md:p-5 max-md:text-[14px] first:mt-0">
             <header className="flex items-center justify-between pb-6">
                 <div className="flex items-center gap-4">
-                    <ImagemPerfil linkUrl={prop.fotoUrl}/> 
+                    <ImagemPerfil linkUrl={props.fotoUrl}/> 
                     <div className="flex flex-col">
-                        <strong className="text-zinc-100 leading-6">{prop.nome}</strong>
-                        <span className="text-sm text-zinc-400 leading-6">{prop.cargo}</span>
+                        <strong className="text-zinc-100 leading-6">{props.nome}</strong>
+                        <span className="text-sm text-zinc-400 leading-6">{props.cargo}</span>
                     </div>
                 </div>
                     <time className="text-zinc-400 text-sm" title="05 de Fevereiro às 14:59h" dateTime="05/02/2025 14:59:00">Publicado à 1h</time>
             </header>
             <main className="leading-6 text-zinc-300">
-                <p className="mt-4">{prop.assunto}</p>
+                <p className="mt-4">{props.assunto}</p>
             </main>
             <section className="mt-6">
                 <form id="formulario" className="flex flex-col gap-4 pt-6 pb-5 border-t border-zinc-500 group">
